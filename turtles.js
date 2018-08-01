96065620ec0f2f0f4d2bc2c81c7e6cf7164c4fd9
Cheyenne Garza <35880763+GitHackingCheyBear@users.noreply.github.com>
2018-08-01 13:45:17 -0700

    javascript1

---
+++ turtles.js
@@ -0,0 +1,22 @@
+// var canvas = document.getElementById("myCanvas");
+// var ctx = canvas.getContext("2d");
+//
+// var x = 350;
+// //trash drops from top
+// //var y = canvas.height-950;
+// var y = canvas.height-850;
+// var dx = 0;
+// var dy = +1;
+//
+// function trash() {
+//   console.log("in trash");
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   ctx.beginPath();
+//   ctx.arc(x, y, 10, 0, Math.PI*2);
+//   ctx.fillStyle = "#0095DD";
+//   ctx.fill();
+//   ctx.closePath();
+//   x += dx;
+//   y += dy;
+// }
+// setInterval(trash, 10);
